13e143bb36dd592c43e00113638ac58af5fe83e4
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2024-02-10 05:52:23 +0900

    Update good.js

--- js/good.js
+++ js/good.js
@@ -1,33 +1,33 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  // 送信フォーム、フォーム送信後の処理のための iframe を取得
-  const form = document.querySelector('.like-form');
-  const iframe = document.querySelector('.like-iframe');
+    // 送信フォーム、フォーム送信後の処理のための iframe を取得
+    const form = document.querySelector('.like-form');
+    const iframe = document.querySelector('.like-iframe');
 
-  // フラグ初期化
-  let submitted = false;
+    // フラグ初期化
+    let submitted = false;
 
-  // 送信が完了したらフラグを立てる
-  form.addEventListener('submit', () => {
-    submitted = true;
-  })
+    // 送信が完了したらフラグを立てる
+    form.addEventListener('submit', () => {
+      submitted = true;
+    });
 
-  // iframe の読み込みが完了したら実行
-  iframe.addEventListener('load', () => {
+    // iframe の読み込みが完了したら実行
+    iframe.addEventListener('load', () => {
 
-    // 送信完了後の処理
-    if(submitted){
+      // 送信完了後の処理
+      if(submitted){
 
-      // 送信ボタン取得
-      const button = document.querySelector('.like-button');
+        // 送信ボタン取得
+        const button = document.querySelector('.like-button');
 
-      // いいねボタン無効化、内部テキスト変更
-      button.disabled = true;
-      button.innerHTML = '<i class="fa-solid fa-thumbs-up fa-lg"></i>';
+        // いいねボタン無効化、内部アイコンクラス変更
+        button.disabled = true;
+        button.querySelector('i').classList.remove('fa-regular');
+        button.querySelector('i').classList.add('fa-solid');
 
-      // 送信メッセージ表示
-      alert('いいねありがとうございました！！');
-
-    }
-  })
-})
+        // 送信メッセージ表示
+        alert('いいねありがとうございました！！');
+      }
+    });
+  });
