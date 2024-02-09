14f027ac5878ff14debb4b01c3b9511fbc56df64
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2024-02-10 05:31:23 +0900

    Update hashtag.js

--- js/hashtag.js
+++ js/hashtag.js
@@ -1,5 +1,6 @@
+//<![CDATA[
 document.querySelectorAll('.label-size, .status-msg-body, .label-info, .breadcrumbs').forEach(function(item) {
 let tmp = item.innerHTML;
 tmp = tmp.replace(/#/g, "");
 item.innerHTML = tmp;
-});
+});//]]>
