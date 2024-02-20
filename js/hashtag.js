79f7cc3d3a0ef9ce19cf46075e100d8e5abb2aee
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2024-02-20 11:47:59 +0900

    Update hashtag.js

--- js/hashtag.js
+++ js/hashtag.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-        // <b>タグ内のテキストから#を除去する処理
-        var boldElements = document.querySelectorAll("b");
-        boldElements.forEach(function(element) {
-            var text = element.textContent;
-            var textWithoutHash = text.replace(/#/g, "");
-            element.innerHTML = textWithoutHash;
-        });
-        // 指定された要素の内部のテキストから#を除去する処理
-        document.querySelectorAll('.label-size, .status-msg-body, .breadcrumbs').forEach(function(item) {
-            var tmp = item.innerHTML;
-            tmp = tmp.replace(/#/g, "");
-            item.innerHTML = tmp;
-        });
-    });
+   // <b>タグ内のテキストから#を除去する処理
+   var boldElements = document.querySelectorAll("b");
+   boldElements.forEach(function(element) {
+      var text = element.textContent;
+      var textWithoutHash = text.replace(/#/g, "");
+      element.innerHTML = textWithoutHash;
+   });
+   // 指定された要素の内部のテキストから#を除去する処理
+   document.querySelectorAll('.label-size, .status-msg-body, .breadcrumbs').forEach(function(item) {
+      var tmp = item.innerHTML;
+      tmp = tmp.replace(/#/g, "");
+      item.innerHTML = tmp;
+   });
+});
