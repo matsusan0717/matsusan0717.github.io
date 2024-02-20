6f91946fd6080ddc20d7c1aa15d8887785bfa0ae
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2024-02-20 11:24:20 +0900

    Update hashtag.js

--- js/hashtag.js
+++ js/hashtag.js
@@ -1,8 +1,17 @@
-<script>
+<script type="text/javascript">
 //<![CDATA[
-document.querySelectorAll('.label-size, .status-msg-body, .breadcrumbs').forEach(function(item) {
-let tmp = item.innerHTML;
-tmp = tmp.replace(/#/g, "");
-item.innerHTML = tmp;
-});//]]>
+    document.addEventListener("DOMContentLoaded", function() {       
+        var boldElements = document.querySelectorAll("b");
+        boldElements.forEach(function(element) {
+            var text = element.textContent;
+            var textWithoutHash = text.replace(/#/g, "");
+            element.innerHTML = textWithoutHash;
+        });
+    document.querySelectorAll('.label-size, .status-msg-body, .breadcrumbs').forEach(function(item) {
+        var tmp = item.innerHTML;
+        tmp = tmp.replace(/#/g, "");
+        item.innerHTML = tmp;
+        });
+    });
+//]]>
 </script>
