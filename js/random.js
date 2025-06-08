80f51d4fb446c2d8d96aab0a3f67f762c52fbf6f
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2025-06-08 20:35:51 +0900

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,3 +1,4 @@
+/*<![CDATA[*/ 
 $(document).ready(function() {
     var items1 = $('.random-item1'); // .random-item1 要素を取得
     var items2 = $('.random-item2'); // .random-item2 要素を取得
@@ -14,3 +15,4 @@ $(document).ready(function() {
     items1.eq(randomIndex1).show();
     items2.eq(randomIndex2).show();
 });
+/*]]>*/
