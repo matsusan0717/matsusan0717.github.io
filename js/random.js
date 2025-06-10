77850967d04ebb76150562d11654b6beccfe99f6
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2025-06-10 19:39:53 +0900

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,26 +1,35 @@
 /*<![CDATA[*/
 $(document).ready(function() {
-    // ランダム1のグループ内の全記事要素を取得
-    var items1 = $('.random-item1-group > .random-item1'); // ★ここを変更！
+    // 現在のページのURLを取得
+    var currentUrl = window.location.href;
 
-    // ランダム2の全記事要素を取得 (こちらは現状のまま)
+    // トップページのURL
+    var homepageUrl = 'https://www.performancedoll.com/';
+
+    // ランダム1のグループ内の全記事要素を取得
+    var items1 = $('.random-item1-group > .random-item1');
+    // ランダム2の全記事要素を取得
     var items2 = $('.random-item2');
 
-    // 初期表示のちらつき防止のため、CSSで display: none; を設定していることを前提とします。
-    // そのため、JavaScriptでの hide() 呼び出しは不要です。
-    // items1.hide(); // 不要
-    // items2.hide(); // 不要
-
-    // random-item1 の中からランダムなインデックスを取得し、該当要素を表示
-    if (items1.length > 0) { // 念のため、要素が存在するかチェック
-        var randomIndex1 = Math.floor(Math.random() * items1.length);
-        items1.eq(randomIndex1).show();
+    // トップページの場合 (ランダム1とランダム2の両方を処理)
+    if (currentUrl === homepageUrl || currentUrl === homepageUrl + 'index.html') { // index.html も考慮
+        if (items1.length > 0) {
+            var randomIndex1 = Math.floor(Math.random() * items1.length);
+            items1.eq(randomIndex1).show();
+        }
+        if (items2.length > 0) {
+            var randomIndex2 = Math.floor(Math.random() * items2.length);
+            items2.eq(randomIndex2).show();
+        }
     }
-
-    // random-item2 の中からランダムなインデックスを取得し、該当要素を表示
-    if (items2.length > 0) { // 念のため、要素が存在するかチェック
-        var randomIndex2 = Math.floor(Math.random() * items2.length);
-        items2.eq(randomIndex2).show();
+    // トップページ以外の場合 (ランダム1のみ処理)
+    else {
+        if (items1.length > 0) {
+            var randomIndex1 = Math.floor(Math.random() * items1.length);
+            items1.eq(randomIndex1).show();
+            // トップページ以外でランダム2が存在する場合、非表示を徹底する
+            items2.hide();
+        }
     }
 });
 /*]]>*/
