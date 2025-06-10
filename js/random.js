4ea216b40d950cb259c0fb56e69425a29183eef3
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2025-06-10 19:16:56 +0900

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,18 +1,16 @@
-/*<![CDATA[*/ 
+/*<![CDATA[*/
 $(document).ready(function() {
-    var items1 = $('.random-item1'); // .random-item1 要素を取得
-    var items2 = $('.random-item2'); // .random-item2 要素を取得
+    var items1 = $('.random-item1');
+    var items2 = $('.random-item2');
 
-    // すべての要素を非表示にする
-    items1.hide();
-    items2.hide();
+    // すべての要素を非表示にする処理は不要になる（CSSで既に非表示）
 
-    // ランダムなインデックスを取得
+    // random-item1 からランダムなインデックスを取得し、表示
     var randomIndex1 = Math.floor(Math.random() * items1.length);
-    var randomIndex2 = Math.floor(Math.random() * items2.length);
-
-    // ランダムに選ばれた要素を表示
     items1.eq(randomIndex1).show();
+
+    // random-item2 からランダムなインデックスを取得し、表示
+    var randomIndex2 = Math.floor(Math.random() * items2.length);
     items2.eq(randomIndex2).show();
 });
 /*]]>*/
