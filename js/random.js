9af70e5d112117a3b15e0e0907a35d355830bd1d
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2025-06-10 20:37:58 +0900

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,23 +1,14 @@
-$(document).ready(function() {
-    // .random-item1 要素を取得
-    var items1 = $('.random-item1'); 
-    // .random-item2 要素を取得
-    var items2 = $('.random-item2'); 
+document.addEventListener("DOMContentLoaded", function() {
+    var items1 = document.querySelectorAll('.random-item1');
+    var items2 = document.querySelectorAll('.random-item2');
 
-    // すべての要素を非表示にする
-    items1.hide(); 
-    items2.hide(); 
-
-    // ランダムなインデックスを取得
-    var randomIndex1 = Math.floor(Math.random() * items1.length); 
-    var randomIndex2 = Math.floor(Math.random() * items2.length); 
-
-    // ランダムに選ばれた要素を表示
-    // 要素が存在する場合のみ処理 (念のため追加)
-    if (items1.length > 0) { 
-        items1.eq(randomIndex1).show(); 
+    if(items1.length > 0){
+        var randomIndex1 = Math.floor(Math.random() * items1.length);
+        items1[randomIndex1].style.display = 'block';
     }
-    if (items2.length > 0) { 
-        items2.eq(randomIndex2).show(); 
+
+    if(items2.length > 0){
+        var randomIndex2 = Math.floor(Math.random() * items2.length);
+        items2[randomIndex2].style.display = 'block';
     }
-}); 
+});
