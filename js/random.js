f240f62014304814871baa01881d8da2e7783606
matsusan0717 <159520386+matsusan0717@users.noreply.github.com>
2025-06-10 20:06:02 +0900

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,35 +1,23 @@
-/*<![CDATA[*/ 
-
+/*<![CDATA[*/
 $(document).ready(function() {
+    var items1 = $('.random-item1');
+    var items2 = $('.random-item2');
 
-    var items1 = $('.random-item1'); // .random-item1 要素を取得
+    // CSSで display: none; または visibility: hidden; が設定されていることを前提とします。
+    // そのため、JavaScriptでの hide() / css('visibility', 'hidden') 呼び出しは不要です。
 
-    var items2 = $('.random-item2'); // .random-item2 要素を取得
+    // ランダムなインデックスを取得
+    var randomIndex1 = Math.floor(Math.random() * items1.length);
+    var randomIndex2 = Math.floor(Math.random() * items2.length);
 
-
-
-    // すべての要素を非表示にする
-
-    items1.hide();
-
-    items2.hide();
-
-
-
-    // ランダムなインデックスを取得
-
-    var randomIndex1 = Math.floor(Math.random() * items1.length);
-
-    var randomIndex2 = Math.floor(Math.random() * items2.length);
-
-
-
-    // ランダムに選ばれた要素を表示
-
-    items1.eq(randomIndex1).show();
-
-    items2.eq(randomIndex2).show();
-
+    // ランダムに選ばれた要素をアニメーションで表示
+    if (items1.length > 0) {
+        items1.eq(randomIndex1).fadeIn(200); // 200ミリ秒でフェードイン
+        // または items1.eq(randomIndex1).css('opacity', 0).animate({opacity: 1}, 200);
+        // (visibility: hidden を使っている場合は css('visibility', 'visible') と併用)
+    }
+    if (items2.length > 0) {
+        items2.eq(randomIndex2).fadeIn(200); // 200ミリ秒でフェードイン
+    }
 });
-
 /*]]>*/
