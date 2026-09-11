417a6cf7aeb336ddbdcef7aa28b90118c5ce2aae
matsusan <159520386+matsusan0717@users.noreply.github.com>
2026-09-11 14:44:27 +0900

    Update tips_custom.js

--- blogger/tips_custom.js
+++ blogger/tips_custom.js
@@ -3,7 +3,7 @@
    ========================================================== */
 
 // 【設定】URL
-const GAS_URL_POST = "https://script.google.com/macros/s/AKfycbyEkKm2obrwrL1l7xBoaPwQMtL53XSCs1SFqctAWAue1tHKkZaeT3QKfmbEwAUHiqSGKg/exec";
+const GAS_URL_POST = "https://script.google.com/macros/s/AKfycbz1r4vTFTaFObmrKFJsin5VjuKxXBdO9sF2AvDmxCHVZ9TjYsxyLbnq-FpLQSOHK_e8Mg/exec";
 const GAS_URL_GET  = "https://script.google.com/macros/s/AKfycby3MRkDpz_QhPwB5scSrxHj1qO9xJo_sugPX9caoJ8nNBThV9SBsZAsouKziHqW16MqPA/exec";
 const BLOG_URL = 'https://blogger.matsusanjpn.com/';
 const EXCLUDE_PATH = "/p/";
